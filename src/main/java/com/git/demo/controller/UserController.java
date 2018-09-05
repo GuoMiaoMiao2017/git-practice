dd6e5b016ced351b312f692f95f60ececddd450a
GmmMiaoMiao2017 <1529548826@qq.com>
2018-09-06 01:11:33 +0800

    第三次实验

--- src/main/java/com/git/demo/controller/UserController.java
+++ src/main/java/com/git/demo/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.git.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
+    @GetMapping("/userInfo")
+    public void info() {
+
+    }
 }
