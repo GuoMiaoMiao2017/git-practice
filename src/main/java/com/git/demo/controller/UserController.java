996e86f28afd1e296e171f738ce02e323516271f
GmmMiaoMiao2017 <1529548826@qq.com>
2018-09-06 01:09:28 +0800

    第二次实验

--- src/main/java/com/git/demo/controller/UserController.java
+++ src/main/java/com/git/demo/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.git.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
+    @PostMapping(value = "/login")
+    public Map<String, Object> login(@RequestBody String name) {
+        //验证用户登录
+        return null;
+    }
 }
